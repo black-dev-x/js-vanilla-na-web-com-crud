2a01b2b2f4ff2373cc2c2589ecb719334712bbfc
black.dev.x <thiago.dev.x.@gmail.com>
2022-04-05 15:17:00 -0300

    aula 02

--- services/client-service.js
+++ services/client-service.js
@@ -1,6 +1,34 @@
-const httpRequest = new XMLHttpRequest()
 const serverAddress = 'http://localhost:3000'
 
-httpRequest.open('GET', `${serverAddress}/profile`)
-httpRequest.send()
-httpRequest.onload = () => console.log(httpRequest.response)
+const loadClientProfiles = () => {
+  fetch(`${serverAddress}/profile`)
+    .then(resposta => resposta.json())
+    .then(profiles => appendToTable(profiles))
+}
+
+const appendToTable = profiles => {
+  const table = document.querySelector('[data-tabela]')
+  profiles.forEach(profile => {
+    const tableRow = newTableRow(profile.nome, profile.email)
+    table.appendChild(tableRow)
+  })
+}
+const newTableRow = (nome, email) => {
+  const tableRow = document.createElement('tr')
+  tableRow.innerHTML = `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li>
+                <a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a>
+            </li>
+            <li>
+                <button class="botao-simples botao-simples--excluir" type="button">Excluir</button>
+            </li>
+        </ul>
+    </td>`
+  return tableRow
+}
+
+loadClientProfiles()
